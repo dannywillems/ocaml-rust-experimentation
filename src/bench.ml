ea458e860af2f3384f0c59274fcf9406995b4d56
Danny Willems <be.danny.willems@gmail.com>
2023-04-26 19:10:06 +0200

    Run dune build @fmt --auto-promote

--- src/bench.ml
+++ src/bench.ml
@@ -1,6 +1,9 @@
 open Core_bench
 
-let bench_rust = Bench.Test.create ~name:"Bench Rust unit" (fun () -> Stubs_rust.unit_call ())
-let bench_c = Bench.Test.create ~name:"Bench C unit" (fun () -> Stubs_c.unit_call ())
+let bench_rust =
+  Bench.Test.create ~name:"Bench Rust unit" (fun () -> Stubs_rust.unit_call ())
 
-let () = Command_unix.run (Core_bench.Bench.make_command [ bench_rust; bench_c ])
+let bench_c =
+  Bench.Test.create ~name:"Bench C unit" (fun () -> Stubs_c.unit_call ())
+
+let () = Command_unix.run (Core_bench.Bench.make_command [bench_rust; bench_c])
